588276a18c50bf9ff3b0326ded34b1d1eec27f94
kwon0koang <kwon0koang@hanwha.com>
2023-04-06 17:04:03 +0900

    [kyk] 필요없는 주석 제거

--- lib/src/repository/todo_repository.dart
+++ lib/src/repository/todo_repository.dart
@@ -11,18 +11,10 @@ part 'todo_repository.g.dart';
 @riverpod
 TodoRepository todoRepository(TodoRepositoryRef ref) => TodoRepository(ref);
 
-// 마이데이터 api retrofit형식으로 정의
 @RestApi()
 abstract class TodoRepository {
   factory TodoRepository(Ref ref) => _TodoRepository(ref.read(dioProvider));
 
   @GET('/todos')
-  // Future<List<TodoModel>> getTodos(@Queries() Map<String, dynamic> queries);
   Future<List<TodoModel>> getTodos();
-
-  // @GET('get_time.cmd')
-  // Future<ServerTimeResp> serverTime();
-
-  // @GET('get_data.cmd')
-  // Future getData(@Queries() Map<String, dynamic> queries);
 }
