3460ac3502add0684983586958ce7b1cfc66a307
kwon0koang <kwon0koang@hanwha.com>
2023-05-22 14:16:57 +0900

    [kyk] cgrw

---
+++ cgrw.sh
@@ -0,0 +1,6 @@
+# sudo rm -r /Users/$myuname/.pub-cache   
+# sudo rm -r ./pubspec.lock 
+flutter clean
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub run build_runner watch
