7b97eec31b2798cfa384ae08380dbe76750bb285
kwon0koang <kwon0koang@gmaill.com>
2023-08-16 21:25:41 +0900

    [kyk] sh 수정

--- sh/buildWebAndPush.sh
+++ sh/buildWebAndPush.sh
@@ -1,8 +1,10 @@
 # 빌드 후 kwon0koang.github.io/test_flutter 푸시
+# /Users/user/kykdev/FlutterProjects/test_flutter
+# /Users/user/kykdev/5000_EtcProject/kwon0koang.github.io
 
 flutter build web --web-renderer canvaskit -t lib/main.dart
-cp -r /Users/user/kykdev/FlutterProjects/test_flutter/build/web/* /Users/user/kykdev/5000_EtcProject/kwon0koang.github.io/test_flutter
-cd /Users/user/kykdev/5000_EtcProject/kwon0koang.github.io/test_flutter
+cp -r /Users/user/kykdev/FlutterProjects/test_flutter/build/web/* /Users/user/kykdev/5000_EtcProject/kwon0koang.github.io
+cd /Users/user/kykdev/5000_EtcProject/kwon0koang.github.io
 git checkout main
 git add .
 git commit -m "[kyk] test_flutter"
