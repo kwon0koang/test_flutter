e60a6c44e641730598990e192db87a4e0489b3b0
kwon0koang <kwon0koang@gmail.com>
2023-11-14 00:00:50 +0900

    [kyk] ios 수정

--- sh/grw.sh
+++
@@ -1,3 +0,0 @@
-flutter pub get
-flutter pub run build_runner build --delete-conflicting-outputs
-flutter pub run build_runner watch
---
+++ sh/gw.sh
@@ -0,0 +1,3 @@
+flutter pub get
+# flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub run build_runner watch
